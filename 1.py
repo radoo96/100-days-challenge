538d37c70a30f563da64fa9302bb06c50cd28299
radoo96 <radekszydziak@gmail.com>
2026-06-21 21:46:26 +0200

    First day

---
+++ 1.py
@@ -0,0 +1,9 @@
+postac = "Rumcajs"
+print(postac)
+
+print(f"Postać ma na imię: {postac}")
+
+imie = input("Jak masz na imię?")
+cel = input("Jaki chcesz osiągnać cel?")
+
+print(f"Siema {imie} :D Twoj cel to {cel}")
