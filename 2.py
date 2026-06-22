e626e66b2e49266b545572f29d67219ff841a32e
radoo96 <radekszydziak@gmail.com>
2026-06-22 21:56:52 +0200

    Day2

---
+++ 2.py
@@ -0,0 +1,15 @@
+#Tak nie rób, skleja dwa printy. Input zwraca string
+
+a = input("Podaj pierwszą liczbę: ")
+b = input("Podaj pierwszą liczbę: ")
+print(a + b)
+
+#Tak rób
+
+a = int(input("Podaj pierwszą liczbę: "))
+b = int(input("Podaj drugą liczbę "))
+print(a + b)
+
+godziny = int(input("Ile godzin przeznaczysz na naukę tygodniowo? "))
+czas = godziny * 52
+print(f"Przy {godziny} tygodniowo to: {czas} godzin")
